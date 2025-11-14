a3940caf76c52b7ba9b2433deda5b39a13037c97
khaled <khaled.bouhabel05@gmail.com>
2025-11-14 13:51:46 +0100

    made so if a user reroutes it scrolls to the top of the page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { useEffect } from "react";
 import Navigation from "@/components/Navigation";
 import Home from "@/pages/Home";
 import Booking from "@/pages/Booking";
@@ -11,6 +12,13 @@ import Contact from "@/pages/Contact";
 import NotFound from "@/pages/not-found";
 
 function Router() {
+  const [location] = useLocation();
+
+  // Scroll to top whenever the route changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <Switch>
       <Route path="/" component={Home} />
